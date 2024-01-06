4651b290e0098a7af9ab0d02420d2c44ddb9279c
azegha <loicanicetazegha@gmail.com>
2024-01-06 14:42:40 +0100

    merci

---
+++ src/main/java/com/example/lovecodeakalo/LovecodeakaloApplication.java
@@ -0,0 +1,13 @@
+package com.example.lovecodeakalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovecodeakaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovecodeakaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lovecodeakalo/LovecodeakaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lovecodeakalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovecodeakaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
