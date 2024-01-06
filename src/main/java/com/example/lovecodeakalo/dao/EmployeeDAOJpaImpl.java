429122cf67ed01b2f654d050bbb77360dc6507ed
azegha <loicanicetazegha@gmail.com>
2024-01-06 15:31:25 +0100

    test

--- src/main/java/com/example/lovecodeakalo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/example/lovecodeakalo/dao/EmployeeDAOJpaImpl.java
@@ -3,6 +3,7 @@ package com.example.lovecodeakalo.dao;
 import com.example.lovecodeakalo.entity.Employee;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -25,5 +26,28 @@ public class EmployeeDAOJpaImpl implements  EmployeeDAO{
 
      List<Employee> employees = theEmployees.getResultList();
         return employees;
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Employee employee = entityManager.find(Employee.class, theId);
+        return employee;
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee theEmployee) {
+        Employee  dbEmployee = entityManager.merge(theEmployee);
+
+        return dbEmployee;
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+Employee employee = entityManager.find(Employee.class, theId);
+
+entityManager.remove(employee);
+
     }
 }
