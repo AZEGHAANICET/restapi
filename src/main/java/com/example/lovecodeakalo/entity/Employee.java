aa6472f3e60aa1fb734e7b49e3dfbec1d19f60c2
azegha <loicanicetazegha@gmail.com>
2024-01-06 14:58:55 +0100

    test

--- src/main/java/com/example/lovecodeakalo/entity/Employee.java
+++ src/main/java/com/example/lovecodeakalo/entity/Employee.java
@@ -24,6 +24,46 @@ public class Employee {
 
 
     // define constructor
+     public Employee(){
+
+     }
+
+    public Employee(String lastName, String firstName, String email) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
 
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
